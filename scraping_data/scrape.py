45c2a100982d3b809c48d08dfa16b90726335034
Nguyen Thanh Tien <31828933+fxtiennguyen92@users.noreply.github.com>
2024-02-05 19:09:34 +0100

    Add footprints dataset input
    Footprints input

--- scraping_data/scrape.py
+++ scraping_data/scrape.py
@@ -28,19 +28,25 @@ def download_image(url, folder):
 # base_path = "dataset/roomlayouts"
         
 ###--- Furnishing ---###
-input_url = "-outputs-inputs.png"
-output_url = "-outputs-outputs.png"
-base_url = "https://stanislaschaillou.com/thesis/GAN/unit_furnishing_results/images/"
-ifrom = 140
-ito = 168
-base_path = "dataset/furnishing"
+# input_url = "-outputs-inputs.png"
+# output_url = "-outputs-outputs.png"
+# base_url = "https://stanislaschaillou.com/thesis/GAN/unit_furnishing_results/images/"
+# ifrom = 140
+# ito = 168
+# base_path = "dataset/furnishing"
 
+###--- Footprints ---###
+input_url = "-inputs.png"
+base_url = "https://stanislaschaillou.com/thesis/GAN/images/"
+ifrom = 501
+ito = 600
+base_path = "dataset/footprints"
 
 for i in range(ifrom, ito):
     base_input_url = base_url + str(i) + input_url
     # Download the image
     download_image(base_input_url, base_path + "/inputs")  
     
-    base_output_url = base_url + str(i) + output_url
-    download_image(base_output_url, base_path + "/outputs")
+    #base_output_url = base_url + str(i) + output_url
+    #download_image(base_output_url, base_path + "/outputs")
 
